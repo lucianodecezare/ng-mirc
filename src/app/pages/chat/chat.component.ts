dd9904bf6bf14f23049b16effb5e63267e5d588a
Luciano de Cezare <lucianodecezare@gmail.com>
2024-05-30 03:36:33 -0300

    Added Logout At Chat Page

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services';
+import { Router } from '@angular/router';
 
 @Component({
-  imports: [],
   selector: 'app-chat',
   standalone: true,
   styleUrl: './chat.component.scss',
   templateUrl: './chat.component.html',
 })
-export class ChatComponent {}
+export class ChatComponent {
+  private authService = inject(AuthService);
+
+  private router = inject(Router);
+
+  public handleLogOut(): void {
+    this.authService
+      .signOut()
+      .then(() => {
+        this.router.navigate(['/login']);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  }
+}

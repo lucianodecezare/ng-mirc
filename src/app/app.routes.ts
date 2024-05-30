fb9d93e1d10280153399cf8ec822d9bc7d83c8a3
Luciano de Cezare <lucianodecezare@gmail.com>
2024-05-30 02:07:21 -0300

    Added Chat And Login Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    loadComponent: () =>
+      import('./pages/chat/chat.component').then((c) => c.ChatComponent),
+    path: 'chat',
+  },
+  {
+    loadComponent: () =>
+      import('./pages/login/login.component').then((c) => c.LoginComponent),
+    path: 'login',
+  },
+  {
+    loadComponent: () =>
+      import('./pages/login/login.component').then((c) => c.LoginComponent),
+    path: '',
+  },
+];

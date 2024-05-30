85d81a00a625875d809709a2977c4674dbf6a029
Luciano de Cezare <lucianodecezare@gmail.com>
2024-05-30 02:00:58 -0300

    Added Chat Page

---
+++ src/app/pages/chat/chat.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-chat',
+  standalone: true,
+  styleUrl: './chat.component.scss',
+  templateUrl: './chat.component.html',
+})
+export class ChatComponent {}

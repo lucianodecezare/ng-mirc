b7d5eefc0f44f1e34e9626485b0cc4231b861566
Luciano de Cezare <lucianodecezare@gmail.com>
2024-05-30 03:28:53 -0300

    Improved Auth Service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Provider, SupabaseClient, createClient } from '@supabase/supabase-js';
 import { environment } from '../../environments/environment.development';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -8,12 +9,30 @@ import { environment } from '../../environments/environment.development';
 export class AuthService {
   private supaBase!: SupabaseClient;
 
+  private router = inject(Router);
+
   constructor() {
     const { supaBaseKey, supaBaseUrl } = environment;
 
     this.supaBase = createClient(supaBaseUrl, supaBaseKey);
 
-    this.supaBase.auth.onAuthStateChange((event, session) => {});
+    this.supaBase.auth.onAuthStateChange((event, session) => {
+      localStorage.setItem('session', JSON.stringify(session?.user));
+
+      if (session?.user) {
+        this.router.navigate(['/chat']);
+      }
+    });
+  }
+
+  get isLoggedIn(): boolean {
+    const user = localStorage.getItem('session');
+
+    if (user) {
+      return true;
+    }
+
+    return false;
   }
 
   public async signInWithProvider(provider: Provider): Promise<void> {

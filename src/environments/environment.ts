abf268f1c279a3a83246d2c1b36a29de19553fff
Luciano de Cezare <lucianodecezare@gmail.com>
2024-05-30 02:29:08 -0300

    Added Environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+// Create a copy of this file with a desired environment like 'environment.development.ts'
+
+export const environment = {
+  production: false,
+  supaBaseKey: '',
+  supaBaseUrl: '',
+};

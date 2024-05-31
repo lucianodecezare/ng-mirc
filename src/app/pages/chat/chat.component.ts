a4398f1223ec22aaad2819a43c886d7b71d26ac5
Luciano de Cezare <lucianodecezare@gmail.com>
2024-05-31 07:49:25 -0300

    Updated Chat Page

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,8 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services';
 import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ChatService } from '../../services/chat.service';
 
 @Component({
+  imports: [ReactiveFormsModule],
+  providers: [ChatService],
   selector: 'app-chat',
   standalone: true,
   styleUrl: './chat.component.scss',
@@ -11,7 +20,19 @@ import { Router } from '@angular/router';
 export class ChatComponent {
   private authService = inject(AuthService);
 
+  private chatService = inject(ChatService);
+
+  private formBuilder = inject(FormBuilder);
+
   private router = inject(Router);
+
+  public chatForm!: FormGroup;
+
+  constructor() {
+    this.chatForm = this.formBuilder.group({
+      chat_message: ['', Validators.required],
+    });
+  }
 
   public handleLogOut(): void {
     this.authService
@@ -21,6 +42,20 @@ export class ChatComponent {
       })
       .catch((error) => {
         alert(error.message);
+      });
+  }
+
+  public onSubmit() {
+    const { chatForm, chatService } = this;
+    const { chat_message } = chatForm.value;
+
+    chatService
+      .chatMessage(chat_message)
+      .then((res) => {
+        chatForm.reset();
+      })
+      .catch((error) => {
+        alert(error);
       });
   }
 }

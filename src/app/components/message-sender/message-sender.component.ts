2c820192ad33ccd0204ac991266b1073c09097b5
Luciano de Cezare <lucianodecezare@gmail.com>
2024-06-06 22:03:04 -0300

    Added Message Sender Component

---
+++ src/app/components/message-sender/message-sender.component.ts
@@ -0,0 +1,28 @@
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  inject,
+} from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  imports: [ReactiveFormsModule],
+  selector: 'app-message-sender',
+  standalone: true,
+  styleUrls: ['./message-sender.component.scss'],
+  templateUrl: './message-sender.component.html',
+})
+export class MessageSenderComponent {
+  @Input() chatForm!: FormGroup;
+
+  @Output() onSubmit = new EventEmitter<any>();
+
+  private changeDetector = inject(ChangeDetectorRef);
+
+  public tempValidatorRequired() {
+    this.changeDetector.detectChanges();
+  }
+}

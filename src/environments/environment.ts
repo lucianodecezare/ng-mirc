dcb090cd422daa05cc673e404c3eafaa3cca3d07
Luciano de Cezare <lucianodecezare@gmail.com>
2024-06-06 21:21:27 -0300

    Removed Vercel Tests

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  supaBaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3c2J4cHRnaGZlaXBkcHZuY2pwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5NzkyNTMsImV4cCI6MjAzMjU1NTI1M30.O8bqHRcEyuPfYZmUka37Mxf8Ias5bdACA4SN_DhdhG4',
-  supaBaseUrl: 'https://cwsbxptghfeipdpvncjp.supabase.co',
+  supaBaseKey: '',
+  supaBaseUrl: '',
 };
